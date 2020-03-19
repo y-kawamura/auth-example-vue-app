4c340ddf3395d3a053f4a3a9fca7feadc12fe475
y-kawamura <tetora.y@gmail.com>
2020-03-19 17:07:51 +0900

    Redirect login page if no logged in user Redirect dashboard page if logged in user

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,22 @@ import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
+function loggedInRedirectDashboard(to, from, next) {
+  if (localStorage.token) {
+    next('/dashboard');
+  } else {
+    next();
+  }
+}
+
+function isLoggedIn(to, from, next) {
+  if (localStorage.token) {
+    next();
+  } else {
+    next('/login');
+  }
+}
+
 const routes = [
   {
     path: '/',
@@ -14,11 +30,19 @@ const routes = [
     path: '/signup',
     name: 'SignUp',
     component: () => import('../views/SignUp.vue'),
+    beforeEnter: loggedInRedirectDashboard,
   },
   {
     path: '/login',
     name: 'Login',
     component: () => import('../views/Login.vue'),
+    beforeEnter: loggedInRedirectDashboard,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue'),
+    beforeEnter: isLoggedIn,
   },
 ];
 
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Dash Board</h1>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+};
+</script>
+
+<style>
+
+</style>
